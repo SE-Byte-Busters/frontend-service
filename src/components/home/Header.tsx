4f7bf42a93e9c483c6f735ddc6915cf529648cc9
daniyeg <daniyeg2@gmail.com>
2025-03-21 02:50:46 +0330

    fix: fix header style

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,57 +1,55 @@
 "use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-import MaterialSymbol from '../MaterialSymbol';
+import MaterialSymbol from "../MaterialSymbol";
 
 export default function Header() {
   const pathname = usePathname();
   const navItems = [
     { name: "صفحه اصلی", href: "/", logo: "home" },
-    { name: "گزارشات شهری", href: "/reports", logo: "description" },
+    { name: "گزارشات اخیر", href: "/reports", logo: "description" },
     { name: "نقشه", href: "/map", logo: "pin_drop" },
     { name: "تابلوی بهترین‌ها", href: "/leaderboard", logo: "leaderboard" },
   ];
 
   return (
-    <header className="bg-white shadow-md fixed top-0 left-0 w-full z-10">
-      <div className="container mx-auto flex justify-between items-center">
+    <header className="bg-white/60 shadow-md fixed top-0 left-0 w-full z-20 backdrop-blur">
+      <div className="flex justify-between items-center container mx-auto px-6 py-2">
         {/* Logo */}
-        <div className="flex items-center">
-          <img src="/logo.png" alt="CleanCity Logo" className="h-20 w-20" />
-          <span className="text-xl font-bold text-gray-800">CleanCity</span>
+        <div className="flex items-center space-x-6">
+          <img src="/logo.png" alt="CleanCity Logo" className="h-16 w-16" />
+          <span className="text-xl text-black">CleanCity</span>
         </div>
 
         {/* Navigation */}
-        <nav className="bg-red-100 rounded-lg p-4 flex flex-row-reverse">
+        <nav className="flex flex-row-reverse bg-light rounded-lg p-2">
           {navItems.map((item) => (
             <Link
               key={item.name}
               href={item.href}
-              className={
-                `flex items-center rounded-md 
-                                ${pathname === item.href
-                  ? "bg-red-900 text-white"
-                  : "text-black"
-                }`
-              }
+              className={`flex flex-row-reverse items-center rounded-lg transition-colors duration-300 pl-2 py-1
+                ${pathname === item.href
+                  ? "bg-primary text-white hover:text-black"
+                  : "text-gray-600 hover:text-black"
+                }`}
             >
-              <div className="flex items-center">
-                <MaterialSymbol
-                  name={item.logo}
-                />
-                <span>{item.name}</span>
-              </div>
+              <MaterialSymbol name={item.logo} className="mr-2" />
+              <span className="text-sm">{item.name}</span>
             </Link>
           ))}
         </nav>
 
-        {/* Sign Up / Sign In */}
-        <div className="flex items-center">
-          <Link href="/signup" className="bg-green-500 text-white rounded hover:bg-green-600">
-            <MaterialSymbol name="person" />
-            <span>ورود / ثبت نام</span>
+        {/* Authentication */}
+        <div>
+          <Link
+            href="/signup"
+            className="flex flex-row-reverse items-center 
+            bg-accent text-white hover:text-black transition-colors duration-300 rounded-lg pl-4 pr-2 py-2"
+          >
+            <MaterialSymbol name="person" className="ml-2" />
+            <span className="text-sm">ورود / ثبت نام</span>
           </Link>
         </div>
       </div>
