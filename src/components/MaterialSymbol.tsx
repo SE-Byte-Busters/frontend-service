0430d8356ad4fd0dba880d89e8966da896421bb5
daniyeg <daniyeg2@gmail.com>
2025-03-21 10:39:00 +0330

    fix: fix footer styling

--- src/components/MaterialSymbol.tsx
+++ src/components/MaterialSymbol.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 interface MaterialSymbolProps {
-    name: string;
-    className?: string;
+  name: string;
+  className?: string;
 }
 
 const MaterialSymbol: React.FC<MaterialSymbolProps> = ({ name, className = '' }) => {
-    return (
-        <span className={`material-symbols-outlined ${className}`}>{name}</span>
-    );
+  return (
+    <span className={`material-symbols-outlined ${className}`}>{name}</span>
+  );
 };
 
 export default MaterialSymbol;
--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from 'next/link';
+import Image from "next/image";
 
 import MaterialSymbol from '../MaterialSymbol';
 
@@ -13,25 +14,22 @@ export default function Footer() {
   ];
 
   return (
-    <footer className="bg-dark">
+    <footer className="bg-dark py-4">
       <div className="container mx-auto">
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col md:flex-row justify-between items-center mb-6">
           {/* Logo */}
-          <div className="flex items-center">
-            <img src="/logo.png" alt="CleanCity Logo" className="h-20" />
-            <span className="font-bold text-black">CleanCity</span>
+          <div className="flex items-center mb-4 md:mb-0">
+            <Image src="/logo.png" width={256} height={256} alt="CleanCity Logo" className="bg-white rounded-xl h-20 w-20 p-1 mx-5" />
+            <span className="text-black font-bold text-2xl">CleanCity</span>
           </div>
 
           {/* Links */}
-          <nav className="flex flex-row-reverse gap-x-5">
+          <nav className="flex flex-wrap justify-center gap-5" dir="rtl">
             {navItems.map((item) => (
               <Link key={item.name} href={item.href}>
-                <div className="flex flex-row-reverse items-center text-black">
-                  <MaterialSymbol
-                    name={item.logo}
-                    className="mx-2"
-                  />
-                  <span>{item.name}</span>
+                <div className="flex items-center text-black font-bold">
+                  <MaterialSymbol name={item.logo} className="mx-2 !text-[48px]" />
+                  <span className="text-base">{item.name}</span>
                 </div>
               </Link>
             ))}
@@ -39,7 +37,7 @@ export default function Footer() {
         </div>
 
         {/* Copyright notice */}
-        <div className="text-center text-black">
+        <div className="text-center text-black font-bold text-sm" dir="rtl">
           <p>&copy; 2025 CleanCity. تمامی حقوق محفوظ است.</p>
         </div>
       </div>
