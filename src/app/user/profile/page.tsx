da630dc0d6fb6d1a16926782325614b276e1e393
daniyeg <daniyeg2@gmail.com>
2025-04-11 17:45:11 +0330

    feat: add basic sections of profile page

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+const InfoSection = () => {
+  return (
+    <div>
+      <p>Info</p>
+    </div>
+  );
+}
+
+const PasswordSection = () => {
+  return (
+    <div>
+      <p>Password</p>
+    </div>
+  );
+}
+
+const ReviewSection = () => {
+  return (
+    <div>
+      <p>Review</p>
+    </div>
+  );
+}
+
 export default function Profile() {
   return (
     <div>
-      <main>Profile</main>
+      <div className="min-h-screen bg-light">
+          <div className="flex flex-col md:flex-row">
+            <InfoSection />
+            <PasswordSection />
+            <ReviewSection />
+          </div>
+      </div>
     </div>
   );
 }
