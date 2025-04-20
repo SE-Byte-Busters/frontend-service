10caa0dce8b196f3bd9ca6a5bb71a6e588e6cde6
daniyeg <daniyeg2@gmail.com>
2025-04-20 21:59:44 +0330

    fix: take viewport metadata out of metadata tag in app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,19 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
-
 
 export const metadata: Metadata = {
   title: "شهر پاک - Clean City",
   description: "یک برنامه برای مدیریت و نظارت بر پاکیزگی و محیط زیست شهر. با نقشه و اطلاعات لحظه‌ای.",
   keywords: "شهر پاک, نقشه شهر, محیط زیست, مدیریت شهری, پاکیزگی, نظارت بر محیط زیست, برنامه شهری",
-  authors: [{ name: "Next.js Team", url: "https://nextjs.org" }],
-  viewport: "width=device-width, initial-scale=1",
+  // authors: [{ name: "Next.js Team", url: "https://nextjs.org" }],
   openGraph: {
     type: "website",
-    url: "https://your-website-url.com",
+    url: "https://shahriar.thetechverse.ir",
     title: "شهر پاک - Clean City",
     description: "یک برنامه برای مدیریت و نظارت بر پاکیزگی و محیط زیست شهر. با نقشه و اطلاعات لحظه‌ای.",
     images: [
       {
-        url: "https://your-website-url.com/og-image.jpg",
+        url: "https://shahriar.thetechverse.ir/og-image.jpg",
         width: 1200,
         height: 630,
         alt: "Clean City Open Graph Image",
@@ -23,6 +21,11 @@ export const metadata: Metadata = {
     ],
   },
 };
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+}
 
 export default function RootLayout({
   children,
