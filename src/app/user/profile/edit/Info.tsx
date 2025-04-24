7f5a68b18078808886e3e9015338cd43ccafaf5d
daniyeg <daniyeg2@gmail.com>
2025-04-25 01:13:54 +0330

    feat: add sections of the edit profile page

---
+++ src/app/user/profile/edit/Info.tsx
@@ -0,0 +1,7 @@
+export default function InfoSection() {
+  return (
+    <section>
+      <p>info</p>
+    </section>
+  )
+}
--- src/app/user/profile/edit/page.tsx
+++ src/app/user/profile/edit/page.tsx
@@ -1,7 +1,11 @@
+import InfoSection from "./Info";
+import PasswordSection from "./Password";
+
 export default function EditProfile() {
   return (
-    <>
-      <p>profile</p>
-    </>
+    <div className="flex flex-col md:flex-row bg-white sm:bg-light pt-16 sm:pt-20">
+      <InfoSection />
+      <PasswordSection />
+    </div>
   )
 }
