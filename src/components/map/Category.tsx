ce5195d14113f89143a914e1f4e58a2f189bb4a5
daniyeg <daniyeg2@gmail.com>
2025-05-11 18:52:01 +0330

    style: change map components tab size to 2

--- src/components/map/Category.tsx
+++ src/components/map/Category.tsx
@@ -2,87 +2,87 @@ import Image from 'next/image'
 import React from 'react'
 
 const Category = () => {
-    return (
-        <div className='col-span-2 mt-4 w-full'>
+  return (
+    <div className='col-span-2 mt-4 w-full'>
 
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4'>
-                <div className='flex items-center justify-between'>
-                    <label htmlFor="greenSpace" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
-                        <input type="checkbox" hidden name="greenSpace" id="greenSpace" />
-                        <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
-                            <Image src="/images/graphics/leaf1.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
-                        </div>
-                        <div>
-                            <span className='block text-sm md:text-md font-bold transition'>فضای سبز و درختان</span>
-                            <span className='block text-sm md:text-md transition'>آسیب به درختان یا وضعیت نامناسب پارک</span>
-                        </div>
-                    </label>
-                </div>
-                <div className='flex items-center justify-between'>
-                    <label htmlFor="garbage" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
-                        <input type="checkbox" hidden name="garbage" id="garbage" />
-                        <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
-                            <Image src="/images/graphics/trash.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
-                        </div>
-                        <div>
-                            <span className='block text-sm md:text-md font-bold transition'>زباله و نظافت</span>
-                            <span className='block text-sm md:text-md transition'>رها شدن زباله یا سطح های پر و آلوده</span>
-                        </div>
-                    </label>
-                </div>
-                <div className='flex items-center justify-between'>
-                    <label htmlFor="visualPollution" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
-                        <input type="checkbox" hidden name="visualPollution" id="visualPollution" />
-                        <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
-                            <Image src="/images/graphics/paint.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
-                        </div>
-                        <div>
-                            <span className='block text-sm md:text-md font-bold transition'>دیوارنویسی و آلودگی بصری</span>
-                            <span className='block text-sm md:text-md transition'>نوشته ها یا تبلیغات نا زیبا روی دیوار</span>
-                        </div>
-                    </label>
-                </div>
-                <div className='flex items-center justify-between'>
-                    <label htmlFor="lighting" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
-                        <input type="checkbox" hidden name="lighting" id="lighting" />
-                        <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
-                            <Image src="/images/graphics/bulb.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
-                        </div>
-                        <div>
-                            <span className='block text-sm md:text-md font-bold transition'>روشنایی معابر</span>
-                            <span className='block text-sm md:text-md transition'>چراغ خاموش یا نور ناکافی در خیابان</span>
-                        </div>
-                    </label>
-                </div>
-                <div className='flex items-center justify-between'>
-                    <label htmlFor="safety" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
-                        <input type="checkbox" hidden name="safety" id="safety" />
-                        <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
-                            <Image src="/images/graphics/hurdle.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
-                        </div>
-                        <div>
-                            <span className='block text-sm md:text-md font-bold transition'>ایمنی و خطرات شهری</span>
-                            <span className='block text-sm md:text-md transition'>محل ناایمن مثل چاه باز یامانع خطرناک</span>
-                        </div>
-                    </label>
-                </div>
-                <div className='flex items-center justify-between'>
-                    <label htmlFor="injury" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
-                        <input type="checkbox" hidden name="injury" id="injury" />
-                        <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
-                            <Image src="/images/graphics/tools.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
-                        </div>
-                        <div>
-                            <span className='block text-sm md:text-md font-bold transition'>خرابی و آسیب دیدگی</span>
-                            <span className='block text-sm md:text-md transition'>چاله، جدول شکسته یا تجهیزات خراب</span>
-                        </div>
-                    </label>
-                </div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4'>
+        <div className='flex items-center justify-between'>
+          <label htmlFor="greenSpace" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
+            <input type="checkbox" hidden name="greenSpace" id="greenSpace" />
+            <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
+              <Image src="/images/graphics/leaf1.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
             </div>
-            <button type="submit" className='block px-4 py-2 mx-auto my-4 w-1/3 shadow-lg shadow-gray-400  cursor-pointer font-bold text-xl lg:text-2xl rounded-full bg-orange-400 hover:bg-orange-300 transition-colors duration-200'>ثبت گزارش</button>
+            <div>
+              <span className='block text-sm md:text-md font-bold transition'>فضای سبز و درختان</span>
+              <span className='block text-sm md:text-md transition'>آسیب به درختان یا وضعیت نامناسب پارک</span>
+            </div>
+          </label>
         </div>
+        <div className='flex items-center justify-between'>
+          <label htmlFor="garbage" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
+            <input type="checkbox" hidden name="garbage" id="garbage" />
+            <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
+              <Image src="/images/graphics/trash.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
+            </div>
+            <div>
+              <span className='block text-sm md:text-md font-bold transition'>زباله و نظافت</span>
+              <span className='block text-sm md:text-md transition'>رها شدن زباله یا سطح های پر و آلوده</span>
+            </div>
+          </label>
+        </div>
+        <div className='flex items-center justify-between'>
+          <label htmlFor="visualPollution" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
+            <input type="checkbox" hidden name="visualPollution" id="visualPollution" />
+            <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
+              <Image src="/images/graphics/paint.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
+            </div>
+            <div>
+              <span className='block text-sm md:text-md font-bold transition'>دیوارنویسی و آلودگی بصری</span>
+              <span className='block text-sm md:text-md transition'>نوشته ها یا تبلیغات نا زیبا روی دیوار</span>
+            </div>
+          </label>
+        </div>
+        <div className='flex items-center justify-between'>
+          <label htmlFor="lighting" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
+            <input type="checkbox" hidden name="lighting" id="lighting" />
+            <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
+              <Image src="/images/graphics/bulb.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
+            </div>
+            <div>
+              <span className='block text-sm md:text-md font-bold transition'>روشنایی معابر</span>
+              <span className='block text-sm md:text-md transition'>چراغ خاموش یا نور ناکافی در خیابان</span>
+            </div>
+          </label>
+        </div>
+        <div className='flex items-center justify-between'>
+          <label htmlFor="safety" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
+            <input type="checkbox" hidden name="safety" id="safety" />
+            <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
+              <Image src="/images/graphics/hurdle.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
+            </div>
+            <div>
+              <span className='block text-sm md:text-md font-bold transition'>ایمنی و خطرات شهری</span>
+              <span className='block text-sm md:text-md transition'>محل ناایمن مثل چاه باز یامانع خطرناک</span>
+            </div>
+          </label>
+        </div>
+        <div className='flex items-center justify-between'>
+          <label htmlFor="injury" className='checkbox-label flex items-center justify-between gap-4 cursor-pointer'>
+            <input type="checkbox" hidden name="injury" id="injury" />
+            <div className='img-wrapper  p-4 w-[60px] h-[60px] rounded-xl transition'>
+              <Image src="/images/graphics/tools.svg" alt="leaf" width={50} height={50} className='w-full h-full object-cover' />
+            </div>
+            <div>
+              <span className='block text-sm md:text-md font-bold transition'>خرابی و آسیب دیدگی</span>
+              <span className='block text-sm md:text-md transition'>چاله، جدول شکسته یا تجهیزات خراب</span>
+            </div>
+          </label>
+        </div>
+      </div>
+      <button type="submit" className='block px-4 py-2 mx-auto my-4 w-1/3 shadow-lg shadow-gray-400  cursor-pointer font-bold text-xl lg:text-2xl rounded-full bg-orange-400 hover:bg-orange-300 transition-colors duration-200'>ثبت گزارش</button>
+    </div>
 
-    )
+  )
 }
 
 export default Category
--- src/components/map/UnSolvedProblemForm.tsx
+++ src/components/map/UnSolvedProblemForm.tsx
@@ -4,179 +4,179 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 
 interface ReportFormProps {
-    onSubmit?: (data: any) => void;
-    className?: string;
+  onSubmit?: (data: any) => void;
+  className?: string;
 }
 
 const UnSolvedProblemForm: React.FC<ReportFormProps> = ({ onSubmit, className }) => {
-    const totalVotes = 13;
-    const positivePercent = 64;
-    const negativePercent = 100 - positivePercent;
-    const [description, setDescription] = useState("");
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const images = [
-        "/images/special/kharabi.png",
-        "/images/special/kharabi.png",
-        "/images/special/kharabi.png",
+  const totalVotes = 13;
+  const positivePercent = 64;
+  const negativePercent = 100 - positivePercent;
+  const [description, setDescription] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    "/images/special/kharabi.png",
+    "/images/special/kharabi.png",
+    "/images/special/kharabi.png",
 
-    ];
-
-
-    const nextSlide = () => {
-        setCurrentIndex((prev) => (prev + 1) % images.length);
-    };
-
-    const prevSlide = () => {
-        setCurrentIndex((prev) => (prev - 1 + images.length) % images.length);
-    };
-
-    return (
-        <div dir="rtl" className="grid grid-cols-12 gap-6 p-6 bg-[#fff5f3] rounded-xl shadow-sm min-h-screen">
-            {/* بخش اطلاعات و کامنت‌ها */}
-            <div className="col-span-12 md:col-span-7 flex flex-row">
-                <div className="flex justify-between items-start flex-row">
-                    <div>
-
-                        <Image src="/images/icons/priorityHight.png" alt="priorityMiddle" className="m-[10px]" width={200} height={200} />
-                        <p className="text-center text-xl text-[#000000] font-vazirmatn max-w-[80%] mx-auto">
-                            منتظر اقدام یکی از شهریارها هستیم
-                            تو هم می‌تونی اولین گام رو برداری...
-                        </p>
-                        <label className="font-bold text-[24px] text-[#685752] font-vazirmatn m-[10px]">نظرات و پیشنهادات</label>
-
-                        <textarea
-                            className="w-[90%] border border-[#685752] p-3 m-2 rounded-[30px] text-[#685752]"
-                            placeholder="مثلا: ترک عمیق به‌وجود آمده و ترس ریزش پل وجود دارد."
-                            rows={4}
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
+  ];
 
 
-                        <h2 className="flex border-b border-[#685752] pb-6 pt-4 m-2">
-                            <span className="font-bold text-[20px] text-[#685752] font-vazirmatn ml-8">
-                                JAVADI334                            </span>
-                            <span className="font-bold text-[20px] text-[#685752] font-vazirmatn">
-                                واقعا خطرناکه مخصوصاً شب‌ها که دیده نمی‌شه.                            </span>
-                        </h2>
-                        <h2 className="flex border-b border-[#685752] pb-6 pt-4 m-2">
-                            <span className="font-bold text-[20px] text-[#685752] font-vazirmatn ml-8">
-                                saraH12
-                            </span>
-                            <span className="font-bold text-[20px] text-[#685752] font-vazirmatn">
-                                لطفاً یه تابلوی هشدار حداقل بذارید تا رسیدگی شه.                            </span>
-                        </h2>
-                        <h2 className="flex border-b border-[#685752] pb-6 pt-4 m-2">
-                            <span className="font-bold text-[20px] text-[#685752] font-vazirmatn ml-8">
-                                hamed_Gangi24                            </span>
-                            <span className="font-bold text-[20px] text-[#685752] font-vazirmatn">
-                                امروز بارون اومد، چاله پر آب شده بود!                            </span>
-                        </h2>
-                    </div>
+  const nextSlide = () => {
+    setCurrentIndex((prev) => (prev + 1) % images.length);
+  };
 
-                    <Image src="/images/icons/unSolved.png" alt="solved" width={80} height={40} />
-                </div>
+  const prevSlide = () => {
+    setCurrentIndex((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  return (
+    <div dir="rtl" className="grid grid-cols-12 gap-6 p-6 bg-[#fff5f3] rounded-xl shadow-sm min-h-screen">
+      {/* بخش اطلاعات و کامنت‌ها */}
+      <div className="col-span-12 md:col-span-7 flex flex-row">
+        <div className="flex justify-between items-start flex-row">
+          <div>
+
+            <Image src="/images/icons/priorityHight.png" alt="priorityMiddle" className="m-[10px]" width={200} height={200} />
+            <p className="text-center text-xl text-[#000000] font-vazirmatn max-w-[80%] mx-auto">
+              منتظر اقدام یکی از شهریارها هستیم
+              تو هم می‌تونی اولین گام رو برداری...
+            </p>
+            <label className="font-bold text-[24px] text-[#685752] font-vazirmatn m-[10px]">نظرات و پیشنهادات</label>
+
+            <textarea
+              className="w-[90%] border border-[#685752] p-3 m-2 rounded-[30px] text-[#685752]"
+              placeholder="مثلا: ترک عمیق به‌وجود آمده و ترس ریزش پل وجود دارد."
+              rows={4}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+
+
+            <h2 className="flex border-b border-[#685752] pb-6 pt-4 m-2">
+              <span className="font-bold text-[20px] text-[#685752] font-vazirmatn ml-8">
+                JAVADI334              </span>
+              <span className="font-bold text-[20px] text-[#685752] font-vazirmatn">
+                واقعا خطرناکه مخصوصاً شب‌ها که دیده نمی‌شه.              </span>
+            </h2>
+            <h2 className="flex border-b border-[#685752] pb-6 pt-4 m-2">
+              <span className="font-bold text-[20px] text-[#685752] font-vazirmatn ml-8">
+                saraH12
+              </span>
+              <span className="font-bold text-[20px] text-[#685752] font-vazirmatn">
+                لطفاً یه تابلوی هشدار حداقل بذارید تا رسیدگی شه.              </span>
+            </h2>
+            <h2 className="flex border-b border-[#685752] pb-6 pt-4 m-2">
+              <span className="font-bold text-[20px] text-[#685752] font-vazirmatn ml-8">
+                hamed_Gangi24              </span>
+              <span className="font-bold text-[20px] text-[#685752] font-vazirmatn">
+                امروز بارون اومد، چاله پر آب شده بود!              </span>
+            </h2>
+          </div>
+
+          <Image src="/images/icons/unSolved.png" alt="solved" width={80} height={40} />
+        </div>
+      </div>
+
+      {/* بخش تصویر و رأی‌دهی */}
+      <div className="col-span-12 md:col-span-5 space-y-3">
+        {/* تغییر عکس اینجاست */}
+        <div className="relative w-80 h-80 overflow-hidden rounded-xl mx-auto">
+
+          <section className="flex items-center gap-2">
+            <Image src={images[currentIndex]}
+              className="object-cover"
+              alt="report" width={350} height={310} />
+
+          </section>
+
+          <button
+            onClick={prevSlide}
+            className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white rounded-full shadow"
+          >
+            <Image src="/images/icons/LeftArrow.png"
+              className="object-cover"
+              alt="report" width={50} height={50} />
+          </button>
+          <button
+            onClick={nextSlide}
+            className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white rounded-full shadow"
+          >
+            <Image src="/images/icons/RightArrow.png"
+              className="object-cover"
+              alt="report" width={50} height={50} />
+          </button>
+          <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-2">
+            {images.map((_, idx) => (
+              <div
+                key={idx}
+                className={`w-2 h-2 rounded-full ${idx === currentIndex ? "bg-green-400" : "bg-gray-300"
+                  }`}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* نوار رأی‌دهی داینامیک */}
+        <div className="w-full space-y-1">
+          <div className="flex w-full h-8 overflow-hidden rounded-full border border-gray-200 shadow-sm text-sm font-bold mt-[10px]">
+            {/* درصد مخالف */}
+            <span className="text-green-600 font-semibold rounded-full">
+              <Image src="/images/icons/dislike.png" alt="dislike" width={56} height={64} />
+            </span>
+
+            <div
+              className="bg-red-200 text-red-600 flex items-center justify-center"
+              style={{
+                width: `${negativePercent}%`,
+                borderTopLeftRadius: '9999px',
+                borderBottomLeftRadius: '9999px',
+                borderTopRightRadius: 0,
+                borderBottomRightRadius: 0,
+              }}
+            >
+              {Math.round(negativePercent)}٪
             </div>
 
-            {/* بخش تصویر و رأی‌دهی */}
-            <div className="col-span-12 md:col-span-5 space-y-3">
-                {/* تغییر عکس اینجاست */}
-                <div className="relative w-80 h-80 overflow-hidden rounded-xl mx-auto">
-
-                    <section className="flex items-center gap-2">
-                        <Image src={images[currentIndex]}
-                            className="object-cover"
-                            alt="report" width={350} height={310} />
-
-                    </section>
-
-                    <button
-                        onClick={prevSlide}
-                        className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white rounded-full shadow"
-                    >
-                        <Image src="/images/icons/LeftArrow.png"
-                            className="object-cover"
-                            alt="report" width={50} height={50} />
-                    </button>
-                    <button
-                        onClick={nextSlide}
-                        className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white rounded-full shadow"
-                    >
-                        <Image src="/images/icons/RightArrow.png"
-                            className="object-cover"
-                            alt="report" width={50} height={50} />
-                    </button>
-                    <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-2">
-                        {images.map((_, idx) => (
-                            <div
-                                key={idx}
-                                className={`w-2 h-2 rounded-full ${idx === currentIndex ? "bg-green-400" : "bg-gray-300"
-                                    }`}
-                            />
-                        ))}
-                    </div>
-                </div>
-
-                {/* نوار رأی‌دهی داینامیک */}
-                <div className="w-full space-y-1">
-                    <div className="flex w-full h-8 overflow-hidden rounded-full border border-gray-200 shadow-sm text-sm font-bold mt-[10px]">
-                        {/* درصد مخالف */}
-                        <span className="text-green-600 font-semibold rounded-full">
-                            <Image src="/images/icons/dislike.png" alt="dislike" width={56} height={64} />
-                        </span>
-
-                        <div
-                            className="bg-red-200 text-red-600 flex items-center justify-center"
-                            style={{
-                                width: `${negativePercent}%`,
-                                borderTopLeftRadius: '9999px',
-                                borderBottomLeftRadius: '9999px',
-                                borderTopRightRadius: 0,
-                                borderBottomRightRadius: 0,
-                            }}
-                        >
-                            {Math.round(negativePercent)}٪
-                        </div>
-
-                        {/* درصد موافق */}
-                        <div
-                            className="bg-green-200 text-green-600 flex items-center justify-center"
-                            style={{
-                                width: `${positivePercent}%`,
-                                borderTopRightRadius: '9999px',
-                                borderBottomRightRadius: '9999px',
-                                borderTopLeftRadius: 0,
-                                borderBottomLeftRadius: 0,
-                            }}
-                        >
-                            {Math.round(positivePercent)}٪
-                        </div>
-
-                        <span className="text-red-500 font-semibold rounded-full">
-                            <Image src="/images/icons/like.png" alt="like" width={56} height={64} />
-                        </span>
-                    </div>
-
-                    <p className="text-center text-xs text-gray-700">{totalVotes} نفر رای داده‌اند</p>
-
-                    <section className="flex justify-between items-center gap-0 mt-10">
-                        <Image src="/images/icons/profile.png" alt="profile" width={80} height={40} />
-                        <section>
-                            <h2 className="font-bold text-xl text-gray-800 mr-0 mt-4">دیوار شکسته و خراب مغازه</h2>
-                            <h2 className="font-bold text-lg text-gray-800 mr-0">تاریخ ثبت گزارش : ۱۰ بهمن ۱۴۰۳</h2>
-                            <h4 className="font-bold text-sm text-gray-800">hamed_Gangi24</h4>
-                        </section>
-                    </section>
-
-                    <p className="text-[#000000] m-4">
-                        دیوار سمت چپ مغازه پر از ترک‌خوردگی و نوشته‌های زشت شده. هم خطرناکه، هم ظاهر محل رو خراب کرده. لطفاً رسیدگی شه چون توی مسیر عبور بچه‌هاست.
-                    </p>
-
-                    <p className="text-[#000000] text-2xl font-bold m-4">تهران، نرسیده به میدان توحید</p>
-                </div>
+            {/* درصد موافق */}
+            <div
+              className="bg-green-200 text-green-600 flex items-center justify-center"
+              style={{
+                width: `${positivePercent}%`,
+                borderTopRightRadius: '9999px',
+                borderBottomRightRadius: '9999px',
+                borderTopLeftRadius: 0,
+                borderBottomLeftRadius: 0,
+              }}
+            >
+              {Math.round(positivePercent)}٪
             </div>
-        </div >
-    );
+
+            <span className="text-red-500 font-semibold rounded-full">
+              <Image src="/images/icons/like.png" alt="like" width={56} height={64} />
+            </span>
+          </div>
+
+          <p className="text-center text-xs text-gray-700">{totalVotes} نفر رای داده‌اند</p>
+
+          <section className="flex justify-between items-center gap-0 mt-10">
+            <Image src="/images/icons/profile.png" alt="profile" width={80} height={40} />
+            <section>
+              <h2 className="font-bold text-xl text-gray-800 mr-0 mt-4">دیوار شکسته و خراب مغازه</h2>
+              <h2 className="font-bold text-lg text-gray-800 mr-0">تاریخ ثبت گزارش : ۱۰ بهمن ۱۴۰۳</h2>
+              <h4 className="font-bold text-sm text-gray-800">hamed_Gangi24</h4>
+            </section>
+          </section>
+
+          <p className="text-[#000000] m-4">
+            دیوار سمت چپ مغازه پر از ترک‌خوردگی و نوشته‌های زشت شده. هم خطرناکه، هم ظاهر محل رو خراب کرده. لطفاً رسیدگی شه چون توی مسیر عبور بچه‌هاست.
+          </p>
+
+          <p className="text-[#000000] text-2xl font-bold m-4">تهران، نرسیده به میدان توحید</p>
+        </div>
+      </div>
+    </div >
+  );
 };
 
 export default UnSolvedProblemForm;
