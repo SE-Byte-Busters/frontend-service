108410f79d8732ed3d4c81256682cddb051f433d
lAmiR1HosseiNl <araeghi@gmail.com>
2025-05-11 19:08:57 +0330

    Add(admin_panel):Add reports page for admin and fix some routing problem

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
-import { Suspense } from "react";
+import { useEffect, useState, Suspense } from "react";
 
 import DesktopHeader from "./DesktopHeader";
 import MobileHeader from "./MobileHeader";
 import { LucideIconName, Icon } from "@/components/Icon";
 
-const navItems: Array<{
+interface NavItem {
   name: string;
   href: string;
   logo: LucideIconName;
-}> = [
+}
+
+const defaultNavItems: NavItem[] = [
   { name: "صفحه اصلی", href: "/", logo: "Home" },
   { name: "گزارشات اخیر", href: "/report/reports", logo: "ScrollText" },
   { name: "نقشه", href: "/map", logo: "MapPin" },
@@ -18,6 +22,32 @@ const navItems: Array<{
 ];
 
 export default function Header() {
+  const [navItems, setNavItems] = useState<NavItem[]>(defaultNavItems);
+
+  useEffect(() => {
+    const updateNavItemsForAdmin = async () => {
+      const token = localStorage.getItem("token");
+      const role = localStorage.getItem("role");
+
+      if (!token || !role) return;
+
+      try {
+        if (role === "admin") {
+          setNavItems((items) =>
+            items.map((item) => {
+              if (item.name === "گزارشات اخیر") return { ...item, href: "/admin/reports/processed" };
+              return item;
+            })
+          );
+        }
+      } catch (error) {
+        console.error("Error updating profile or nav items", error);
+      }
+    };
+
+    updateNavItemsForAdmin();
+  }, []);
+
   return (
     <header className="bg-white/60 shadow-md fixed top-0 left-0 w-full z-20 backdrop-blur">
       <div className="flex flex-col sm:flex-row justify-between items-center container mx-auto px-4 sm:px-6 gap-2 sm:gap-0">
